f1082dd01e6688bcb71a94ed9bc6958bf15ff7ea
Hannah Kates <hannah.b.kates@gmail.com>
2020-10-08 12:26:38 -0400

    legend styling

--- map.js
+++ map.js
@@ -4,8 +4,10 @@ var dropdown = d3.select("#variable-options");
 // function for loading the data and creating the d3 map
 function renderMap() {
   console.log('render map triggered', new Date().getSeconds());
-  //clear way for the regeneration
-  d3.selectAll("path").remove();
+
+  //clear all g elements for the regeneration of the map and legend
+  d3.selectAll("g").remove();
+
   // make load spinner visible
   document.getElementsByClassName('loader')[0].style.visibility = 'visible';
 
@@ -17,23 +19,25 @@ function renderMap() {
   svg.attr("width", width);
   svg.attr("height", height);
 
+  // TODO: understand what this is doing
   var data = d3.map();
 
-  // Data and color scale
-  var colorScale = d3.scaleThreshold()
-    // domain is based on data value range
-    .domain([.1, .2, .3, .4, .5])
-    .range(d3.schemeBlues[5]);
-
+  // Get the variable currently selected in the dropdown menu
   var variableName = dropdown.node().options[dropdown.node().selectedIndex].value;
   var fieldName
 
+  // Translate the selected variable in the dropdown menu to
+  // 1) the data table field name (fieldName), and
+  // 2) the preferred title for the legend (legendTitle)
   if (variableName == "total") {
     fieldName = "No Internet Access (Percentage of Households)";
+    legendTitle = "% of Households without Any Broadband Access"
   } else if (variableName == "mobile") {
     fieldName = "No Mobile Broadband Adoption (Percentage of Households)";
+    legendTitle = "% of Households without Mobile Broadband"
   } else if (variableName == "home") {
     fieldName = "No Home Broadband Adoption (Percentage of  Households)";
+    legendTitle = "% of Households without Broadband at Home"
   } else {
     console.log('no field selected by user');
   }
@@ -58,6 +62,12 @@ function renderMap() {
         .fitSize([width, height], cds)
       );
 
+    // Data and color scale
+    var colorScale = d3.scaleThreshold()
+      // domain is based on data value range
+      .domain([.1, .2, .3, .4, .5])
+      .range(d3.schemeBlues[5]);
+
     // Draw the map
     svg.append("g")
       .selectAll("path")
@@ -71,6 +81,23 @@ function renderMap() {
         d.total = data.get(d.properties.boro_cd) || 0;
         return colorScale(d.total);
       });
+
+      // Legend
+      var g = svg.append("g")
+          .attr("class", "legendThreshold")
+          .attr("transform", "translate(20,20)");
+      g.append("text")
+          .attr("class", "caption")
+          .attr("x", 0)
+          .attr("y", -6)
+          .text(legendTitle);
+      var labels = ['0-10%', '11-20%', '21-30%', '31-40%', '41-50%'];
+      var legend = d3.legendColor()
+          .labels(function (d) { return labels[d.i]; })
+          .shapePadding(4)
+          .scale(colorScale);
+      svg.select(".legendThreshold")
+          .call(legend);
 
     document.getElementsByClassName('loader')[0].style.visibility = 'hidden';
     console.log('done', new Date().getSeconds());
